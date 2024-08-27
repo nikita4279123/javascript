76342580423bfb2c5eb6d0ce4ab38b81cd2cf053
nikita4279123 <nikitamunda42@gmail.com>
2024-08-27 10:16:15 +0000

    baccground video

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("code");
+const accountId=167354;
+let accountEmail="nikits@43526com.";
+var accountPassword="312124";
+accoountCity="Ranchi";
+
+console.table([accountId,accountId,accountPassword,accoountCity]);
