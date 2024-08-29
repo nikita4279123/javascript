600c95b6afb7fef869f449007e1d770a33513bb3
nikita4279123 <nikitamunda42@gmail.com>
2024-08-29 08:44:48 +0000

    background video

--- app.js
+++
@@ -1,6 +0,0 @@
-const accountId=167354;
-let accountEmail="nikits@43526com.";
-var accountPassword="312124";
-accoountCity="Ranchi";
-
-console.table([accountId,accountId,accountPassword,accoountCity]);
